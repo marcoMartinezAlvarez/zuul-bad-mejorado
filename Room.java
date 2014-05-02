23ea760360870750999ef902eb16f0ba1069e22c
Francisco <kiko_drosa@hotmail.com>
2014-05-02 10:56:15 +0200

    Implementado el método getLongDescription() en la clase Room

--- Room.java
+++ Room.java
@@ -61,11 +61,21 @@ public class Room
     public String getExitString(){
         Set<String> direccionesPosibles = salidas.keySet();
         String posiblesSalidas = "Exits :";
-        
+
         for(String direccion: direccionesPosibles){
             posiblesSalidas += direccion + "";
         }    
-            return posiblesSalidas;
+        return posiblesSalidas;
+    }
+
+    /**
+     * Return a long description of this room, of the form:
+     *     You are in the 'name of room'
+     *     Exits: north west southwest
+     * @return A description of the room, including exits.
+     */
+    public String getLongDescription(){       
+        return "You are in the " + getDescription() + ".\n"  + getExitString();
     }
 }
 
